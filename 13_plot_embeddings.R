baf787c3842bd013d0800819a6fcbd2b867f2de1
Alicia Chen <31644506+aliciamchen@users.noreply.github.com>
2022-08-21 22:24:04 -0400

    Get custom titles to work on 2d embeddings plot

--- 13_plot_embeddings.R
+++ 13_plot_embeddings.R
@@ -5,25 +5,49 @@ library(ggthemes)
 
 theme_set(theme_few(base_size = 15))
 d <- read.csv(here("test_output/embedding_viz.csv"))
+d.metrics <- read.csv((here("test_output/sys_disc_agg.csv")))
+
 
 # Make color mapping vector
 col <- unique(d$referent)
 names(col) <- col
 
-# todo: savecommunication score, discreteness, systematicity per participant
+# Make list of systematicity, discreteness, communication scores (otherwise labeling doesn't work)
+comm_list <- split(d.metrics$comm_score, d.metrics$speaker)
+disc_list <- split(d.metrics$discreteness, d.metrics$speaker)
+sys_list <- split(d.metrics$systematicity, d.metrics$speaker)
 
 # Plot
+facet_titler <-
+  function(string) {
+    paste0(
+      "speaker ",
+      string, 
+      "\n",
+      "comm score ",
+      round(as.numeric(unlist(comm_list[string])), 3), # Jank but doesn't work otherwise
+      "\n",
+      "disc ",
+      round(as.numeric(unlist(disc_list[string])), 3),
+      "\n",
+      "sys ",
+      round(as.numeric(unlist(sys_list[string])), 3) 
+    )
+    
+  }
+
 for (g in unique(d$game)) {
   d.this.game = d %>% filter(game == g)
   
   plot = ggplot(d.this.game, aes(x = mds_1, y = mds_2)) +
-    geom_point(aes(color = referent), size = 1.5, alpha = 0.6) +
-    scale_fill_manual(values = col) + 
-    scale_color_manual(values = col) + 
-    theme(legend.position = "none") + 
-    lims(x = c(-7, 6), y = c(-5, 5)) + 
-    facet_wrap(~speaker)
-  # TODO: set consistent x and y axes
+    geom_point(aes(color = referent, shape = factor(cluster_label)),
+               size = 2,
+               alpha = 0.8) +
+    scale_fill_manual(values = col) +
+    scale_color_manual(values = col) +
+    theme(legend.position = "none") +
+    lims(x = c(-7, 6), y = c(-5, 5)) +
+    facet_wrap(~factor(speaker), labeller = as_labeller(facet_titler))
   
   print(plot)
 }
@@ -43,5 +67,5 @@ ggplot(d, aes(x = mds_1, y = mds_2)) +
   scale_color_manual(values = col) +
   scale_fill_manual(values = col) +
   theme(legend.position = "none") +
-  # coord_fixed() + 
+  # coord_fixed() +
   lims(x = c(-7, 6), y = c(-5, 5))
