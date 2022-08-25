409a1a2488d6618265953544b4ff1c0771c1ae80
Alicia Chen <31644506+aliciamchen@users.noreply.github.com>
2022-08-24 22:47:32 -0400

    update process signals file to take in learn and comm arguments

--- 01_process_signals.py
+++ 01_process_signals.py
@@ -23,6 +23,13 @@ def fetch_json_signal(df, speaker, referent):
     return raw_signal
 
 
+def key_by_value(my_dict, value):
+    """Smol helper function"""
+    keys = list(my_dict.keys())
+    values = list(my_dict.values())
+    return keys[values.index(value)]
+
+
 def make_comm_df(df):
     """Make dataframe of communication phase signals
 
@@ -66,9 +73,11 @@ def make_comm_df(df):
 
 def make_init_df(initialization, init_signals_dir=params.learning_sigs_dir):
     """Make dataframe of initialization signals
+    This indexes the signals by referent ids, so there may be as many signals as referents
+    (i.e. it may repeat signals)
 
     Args:
-        initialization (str)
+        initialization (str): either "one2one" or "one2many"
         init_signals_dir (str, optional): folder to find `.json` file of learning phase signals.
         Defaults to params.learning_sigs_dir.
 
@@ -77,20 +86,21 @@ def make_init_df(initialization, init_signals_dir=params.learning_sigs_dir):
     """
     print("Processing initialization signals...")
 
-    signals = preprocess.load_signals_from_folder(
-        init_signals_dir
-    )  # load from `learning_signals.json`
+    signals = preprocess.load_signals_from_folder(init_signals_dir)[
+        "learning_signals"
+    ]  # load from `learning_signals.json`
 
     all_signals = []
 
-    # because all the signals start in one `.json` file, index using filename
-    for idx, raw_signal in enumerate(signals["learning_signals"]):
+    for referent_id, signal_id in params.init_signal_mappings[initialization].items():
 
         df_signal = preprocess.make_signal_df(
-            raw_signal,
+            signals[signal_id],
             sampling_freq=params.sampling_freq,
-            referent_id=params.init_idx_color_mappings[idx],
-            referent=params.learning_referents[idx],
+            referent_id=referent_id,
+            referent=params.learning_referents[initialization][
+                key_by_value(params.init_color_mappings[initialization], referent_id)
+            ],
             speaker="init",
         )
 
@@ -101,8 +111,15 @@ def make_init_df(initialization, init_signals_dir=params.learning_sigs_dir):
     return big_df
 
 
-def make_learn_df(df):
+def make_learn_df(initialization, df):
     """Make dataframe of learning phase signals
+
+    Args:
+        initialization (str): type of initialization, either "one2many" or "one2one"
+        df (DataFrame): raw learning phase dataframe
+
+    Returns:
+        DataFrame:
     """
     print("Processing learning phase signals...")
     all_signals = []
@@ -122,11 +139,12 @@ def make_learn_df(df):
         if (not raw_signal) or (len(raw_signal) == 1):
             continue
 
+        # print(row.referent_id)
         df_signal = preprocess.make_signal_df(
             raw_signal,
             sampling_freq=params.sampling_freq,
-            referent=params.learning_referents[row.referent_id],
-            referent_id=row.referent_id,
+            referent=params.learning_referents[initialization][row.referent_id],
+            referent_id=params.init_color_mappings[initialization][row.referent_id],
             signal_id=row.signal_id,
             block_id=row.block_id,
             n_correct_lc_guesses=row.num_correc_lc_guesses,
@@ -143,18 +161,51 @@ def make_learn_df(df):
     return big_df
 
 
-if __name__ == "__main__":
-
-    comm_filename = "test_output/communication.zip"
-    learn_filename = "test_output/learning.zip"
+def main(args):
+    comm_filename = args.comm_file
+    learn_filename = args.learn_file
+    label = args.expt_tag
+    output_dir = args.output_dir
 
     df_learn = pd.read_csv(learn_filename)
     df_comm = pd.read_csv(comm_filename)
 
-    df_init = make_init_df()
-    df_learn_long = make_learn_df(df_learn)
+    df_init = make_init_df(label)
+    df_learn_long = make_learn_df(label, df_learn)
     df_comm_long = make_comm_df(df_comm)
 
-    df_init.to_csv("test_output/init_signals.zip", index=False)
-    df_learn_long.to_csv("test_output/learning_signals.zip", index=False)
-    df_comm_long.to_csv("test_output/comm_signals.zip", index=False)
+    df_init.to_csv(os.path.join(output_dir, f"{label}_init_signals.zip"), index=False)
+    df_learn_long.to_csv(
+        os.path.join(output_dir, f"{label}_learn_signals.zip"), index=False
+    )
+    df_comm_long.to_csv(
+        os.path.join(output_dir, f"{label}_comm_signals.zip"), index=False
+    )
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        "--expt_tag",
+        required=True,
+        type=str,
+        help="which experiment? for labeling files",
+    )
+    parser.add_argument(
+        "--learn_file",
+        required=True,
+        type=str,
+        help="file for (excluded) learning data",
+    )
+    parser.add_argument(
+        "--comm_file", required=True, type=str, help="file for (excluded) comm data"
+    )
+    parser.add_argument(
+        "--output_dir", required=True, type=str, help="place to save output"
+    )
+
+    args = parser.parse_args()
+
+    main(args)
+
