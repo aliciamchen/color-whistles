8da1909ce35c086e55e0e9385f1743c677aa7f95
Alicia Chen <31644506+aliciamchen@users.noreply.github.com>
2023-09-23 21:27:00 -0400

    add extra playing around with stats before cleaning

--- 11_stats.R
+++ 11_stats.R
@@ -7,21 +7,35 @@ library(tidyboot)
 library(mgcv)
 library(readr)
 
-theme_set(theme_few(base_size = 15))
+theme_set(theme_classic(base_size = 20))
 
 d <- read.csv(here('test/one2one_sys_disc_agg_new.csv')) %>%
-  mutate(paired_comm_score = (own_score + comm_score) / 2)
+  mutate(paired_comm_score = (own_score + comm_score) / 2, 
+         learn_score_norm = (learn_score - min(learn_score, na.rm = TRUE)) / (max(learn_score, na.rm = TRUE) - min(learn_score, na.rm = TRUE)) ,
+         alignment_norm = (alignment - min(alignment, na.rm = TRUE)) / (max(alignment, na.rm = TRUE) - min(alignment, na.rm = TRUE)))
 
 d.comm <- read_csv(here('test_output/communication.zip'))
 
 
 # relationship between learning and communication score
-ggplot(d, aes(x = learn_score, y = comm_score)) +
-  geom_point() +
-  geom_smooth(method = 'lm') + 
-  labs(title = "learning and communication score")
 
-mod <- lm(comm_score ~ 1 + learn_score,
+  
+ggplot(d, aes(x = learn_score_norm, y = comm_score)) +
+  geom_point(size = 4, alpha = 0.8, stroke = 0) +
+  geom_smooth(method = 'lm', se = TRUE, fill = "lightblue", color = "black") +
+  labs(title = "learning and communication score") +
+  geom_hline(yintercept=0.5, color="red", linetype="dashed") +
+  theme(axis.ticks.x=element_blank(),
+        axis.ticks.y=element_blank()) + 
+  ylim(0.35, 1)
+
+
+ggsave(here("figures/learn_comm.pdf"),
+       width = 5,
+       height = 4.5)
+
+
+mod <- lm(comm_score ~ 1 + learn_score_norm,
           data = d)
 
 summary(mod)
@@ -31,9 +45,22 @@ learn_comm_resid <- rstandard(mod)
 d.with.resid <- cbind(na.omit(d), learn_comm_resid)
 
 # relationship between communication round and communication score
-ggplot(d.comm, aes(x = round, y = score)) +
-  geom_smooth() +
-  labs(title = "one to one initialization")
+d.learn.comm <- d.comm %>% 
+  group_by(round) %>% 
+  tidyboot_mean(score, na.rm = T)
+
+ggplot(d.learn.comm, aes(x = round, y = empirical_stat)) +
+  geom_line() +
+  geom_ribbon(aes(ymin=ci_lower, ymax=ci_upper), alpha=0.2) +
+  geom_hline(yintercept=0.5, color="red", linetype="dashed") +
+  labs(title = "improvement over rounds", y = "communication score") +
+  theme(axis.ticks.x=element_blank(),
+        axis.ticks.y=element_blank()) +
+  ylim(0.35, 1)
+
+ggsave(here("figures/comm_improvement.pdf"),
+       width = 4,
+       height = 4.5)
 
 comm.score.mod <- gam(score ~ s(round, bs = "cs"),
                       data = d.comm)
@@ -41,8 +68,8 @@ comm.score.mod <- gam(score ~ s(round, bs = "cs"),
 summary(comm.score.mod)
 
 # Try linear model (note: doesn't converge)
-comm.score.mod.linear <- lmer(score ~ 1 + round + (1 + round |
-                                                     gameid / speakerid),
+comm.score.mod.linear <- lmer(score ~ 1 + round + (1 |
+                                                     gameid),
                               data = d.comm)
 
 summary(comm.score.mod.linear)
@@ -50,13 +77,67 @@ summary(comm.score.mod.linear)
 
 # relationship between discreteness and systematicity
 ggplot(d, aes(x = systematicity, y = discreteness, col = comm_score)) +
-  geom_point(size = 2.5) + 
-  labs(title = "discreteness and global systematicity")
+  geom_point(size = 3.3) + 
+  labs(title = "discreteness and global systematicity") +
+  theme(axis.ticks.x=element_blank(),
+        axis.ticks.y=element_blank())
+
+ggsave(here("figures/disc_sys_perf.pdf"),
+       width = 6.5,
+       height = 4.5)
 
 disc.sys.mod <- lm(discreteness ~ 1 + systematicity,
                    data = d)
 
 summary(disc.sys.mod)
+
+# histogram of discreteness and systematicity
+ggplot(d, aes(x = systematicity)) + 
+  geom_histogram()
+
+d %>% 
+  summarise(mean = mean(systematicity), sd = sd(systematicity))
+
+d %>% 
+  summarize(mean = mean(discreteness), sd = sd(discreteness))
+
+ggplot(d, aes(x = discreteness)) + 
+  geom_histogram()
+
+# cut off discreteness and systematicity at median
+d.blah <- d %>% 
+  mutate(is_discrete = ifelse(discreteness > 0.75, T, F), 
+         is_systematic = ifelse(systematicity > 0.5, T, F))
+
+# how many discrete and systematic? 
+d.blah %>% 
+  group_by(is_discrete, is_systematic) %>% 
+  count()
+
+# bar plot
+d.bar <- d.blah %>% 
+  group_by(is_discrete, is_systematic) %>% 
+  tidyboot_mean(comm_score, na.rm = T) %>% 
+  mutate(category = case_when(
+    is_discrete & is_systematic ~ "D+S+",
+    is_discrete & !is_systematic ~ "D+S-",
+    !is_discrete & is_systematic ~ "D-S+",
+    !is_discrete & !is_systematic ~ "D-S-",
+    TRUE ~ "UNKNOWN"
+  ))
+
+
+
+ggplot(d.bar, aes(x = category, y = empirical_stat)) +
+  geom_point(mapping = aes(x = category, y = empirical_stat),
+                 size = 2.3,
+                 alpha = 1,
+                 position = position_dodge(width = 0.8)) +
+  geom_errorbar(mapping = aes(x = category, ymin = ci_lower, ymax = ci_upper),
+                    position = position_dodge(width = 0.8),
+                    size = 1.5,
+                    width = 0.09) +
+  labs(y = "communication score")
 
 # relationship between discreteness and within- / between-cluster systematicity
 ggplot(d, aes(x = btwn_clust_sys, y = discreteness, col = comm_score)) +
@@ -67,6 +148,9 @@ ggplot(d, aes(x = within_clust_sys, y = discreteness, col = comm_score)) +
   geom_point(size = 2.5) + 
   labs(title = "discreteness and within-cluster systematicity")
 
+ggplot(d, aes(x = systematicity, y = discreteness, col = comm_score)) +
+  geom_point(size = 2.5) + 
+  labs(title = "discreteness and systematicity")
 
 ggplot(d, aes(x = within_clust_sys, y = btwn_clust_sys, col = comm_score)) + 
   geom_point(size = 2.5) + 
@@ -76,6 +160,10 @@ ggplot(d, aes(x = within_clust_sys, y = btwn_clust_sys, col = comm_score)) +
 ggplot(d, aes(x = within_clust_sys, y = btwn_clust_sys, col = systematicity)) + 
   geom_point(size = 2.5) + 
   labs(title = "within- and between-cluster systematicity vs. global systematicity")
+
+ggplot(d, aes(x = systematicity, y = btwn_clust_sys, col = discreteness)) + 
+  geom_point(size = 2.5) + 
+  labs(title = "")
 
 
 # no relationship between between- and within-cluster systematicity
@@ -97,12 +185,12 @@ summary(perf.mod)
 
 
 # relationship between discreteness, systematicity, and performance
-perf.mod <- lm(scale(comm_score) ~ 1 + scale(discreteness) * scale(systematicity),
+perf.mod <- lmer(scale(comm_score) ~ 1 + scale(discreteness) + scale(systematicity) + (1 | game),
                data = d)
 
 summary(perf.mod)
 
-perf.mod <- lm(scale(comm_score) ~ 1 + scale(discreteness) + scale(systematicity) * scale(within_clust_sys) * scale(btwn_clust_sys),
+perf.mod <- lmer(scale(comm_score) ~ 1 + scale(discreteness) + scale(systematicity) * scale(within_clust_sys) * scale(btwn_clust_sys) + (1 | game),
                data = d)
 
 summary(perf.mod)
@@ -228,3 +316,50 @@ ggplot(d.with.resid,
   geom_point(size = 2.5) +
   theme(legend.position = "none")
 
+
+# does partner alignment for either discreteness or systematicity predicts better communication? NO
+
+d.alignment <- d %>% 
+  group_by(game, paired_comm_score) %>% 
+  summarise(
+    abs_diff_systematicity = abs(diff(systematicity)),
+    abs_diff_discreteness = abs(diff(discreteness)) 
+  )
+
+ggplot(d.alignment, aes(x = abs_diff_systematicity, y = abs_diff_discreteness, col = paired_comm_score)) +
+  geom_point(size = 2.5)
+
+ggplot(d.alignment, aes(x = abs_diff_systematicity, y = paired_comm_score)) +
+  geom_point(size = 2.5)
+
+ggplot(d.alignment, aes(x = abs_diff_discreteness, y = paired_comm_score)) +
+  geom_point(size = 2.5)
+
+ggplot(d.alignment, aes(x = alignment, y = paired_comm_score)) +
+  geom_point(size = 2.5)
+
+mod <- lm(paired_comm_score ~ 1 + abs_diff_discreteness * abs_diff_systematicity,
+                 data = d.alignment)
+
+summary(mod)
+
+# partner alignment for signals 
+ggplot(d %>% distinct(alignment_norm, .keep_all = T), aes(x = alignment_norm, y = paired_comm_score)) + 
+  geom_point(size = 2.5) 
+
+mod <- lm(paired_comm_score ~ alignment_norm, data = d %>% distinct(alignment_norm, .keep_all = T))
+summary(mod) 
+
+ggplot(d, aes(x = alignment, y = comm_score)) + 
+  geom_point(size = 2.5)
+
+mod <- lm(comm_score ~ alignment, data = d)
+summary(mod)
+
+# there isnt an effect. 
+# what about learning score and partner alignment? 
+mod <- lm(alignment ~ learn_score, data = d)
+summary(mod)
+
+ggplot(d, aes(x = alignment, y = learn_score)) + 
+  geom_point(size = 2.5)
