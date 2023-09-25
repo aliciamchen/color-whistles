d3cbd1b2e7b9d8777cfe0b02db285c29218d8424
Alicia Chen <31644506+aliciamchen@users.noreply.github.com>
2023-09-24 20:34:37 -0400

    asdfjlsdkfj

--- 12_plot_embeddings.R
+++ 12_plot_embeddings.R
@@ -23,10 +23,11 @@ facet_titler <-
   function(string) {
     paste0(
       "speaker ",
-      string, 
+      string,
       "\n",
       "comm score ",
-      round(as.numeric(unlist(comm_list[string])), 3), # Jank but doesn't work otherwise
+      round(as.numeric(unlist(comm_list[string])), 3),
+      # Jank but doesn't work otherwise
       "\n",
       "disc ",
       round(as.numeric(unlist(disc_list[string])), 3),
@@ -35,7 +36,7 @@ facet_titler <-
       round(as.numeric(unlist(sys_list[string])), 3),
       "\n",
       "al ",
-      round(as.numeric(unlist(align_list[string])), 3) 
+      round(as.numeric(unlist(align_list[string])), 3)
     )
     
   }
@@ -51,7 +52,7 @@ for (g in unique(d$game)) {
     scale_color_manual(values = col) +
     theme(legend.position = "none") +
     lims(x = c(-7, 6), y = c(-5, 5)) +
-    facet_wrap(~factor(speaker), labeller = as_labeller(facet_titler))
+    facet_wrap( ~ factor(speaker), labeller = as_labeller(facet_titler))
   
   print(plot)
 }
@@ -60,7 +61,12 @@ for (g in unique(d$game)) {
 # Big plot
 
 f = ggplot(d, aes(x = mds_1, y = mds_2)) +
-  geom_point(aes(color = referent), size = 2.7, alpha = 0.5, stroke = 0) +
+  geom_point(
+    aes(color = referent),
+    size = 2.7,
+    alpha = 0.5,
+    stroke = 0
+  ) +
   geom_point(
     data = d %>% filter(speaker == 'init'),
     aes(fill = referent),
@@ -71,13 +77,31 @@ f = ggplot(d, aes(x = mds_1, y = mds_2)) +
   scale_color_manual(values = col) +
   scale_fill_manual(values = col) +
   theme(legend.position = "none") +
-  coord_fixed() +
-  theme(axis.text.x=element_blank(),
-        axis.ticks.x=element_blank(),
-        axis.text.y=element_blank(),
-        axis.ticks.y=element_blank())
-  # lims(x = c(-7, 6), y = c(-5, 5))
+  coord_fixed()
+
+# +
+#   theme(axis.text.x=element_blank(),
+#         axis.ticks.x=element_blank(),
+#         axis.text.y=element_blank(),
+#         axis.ticks.y=element_blank())
+# lims(x = c(-7, 6), y = c(-5, 5))
 
 f
 
-ggsave(here("figures/mds_all.pdf"), f, width = 4, height = 6)
+ggsave(here("figures/mds_all.pdf"),
+       f,
+       width = 4,
+       height = 6)
+
+
+
+# filter df for values of mds_1 between 0.5 and 1.5
+# and values of mds_2 between 0.5 and 2
+
+# filter for yellow colors: referent_id between 7 and 12
+# to pull out for systematicity calculation
+
+df.filtered <-
+  d %>% filter(mds_1 > 0.5 &
+                 mds_1 < 1.5 &
+                 mds_2 > 0.5 & mds_2 < 2 & referent_id > 6 & referent_id < 13)
