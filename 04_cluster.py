e71b87e13bd77ba12162613b3fcf285343e3571d
Alicia Chen <31644506+aliciamchen@users.noreply.github.com>
2024-04-22 17:53:51 -0400

    add new clustering method

--- 04_cluster.py
+++ 04_cluster.py
@@ -4,26 +4,60 @@
 
 import numpy as np
 import pandas as pd
-from sklearn.mixture import BayesianGaussianMixture
+import json
+from sklearn.cluster import HDBSCAN
 import params
 
 
-df = pd.read_csv("test/one2many_embedding_viz.csv")
+# df = pd.read_csv("test_output/embedding_disc.csv")
+
+# %% Load data
+
+# load in matrix of all pairwise distances
+pairwise_dists = np.loadtxt("test_output/pairwise_dists.txt")
+signal_labels = pd.read_json("test_output/all_signal_labels.json")
+signal_labels.columns = ["game", "speaker", "referent", "referent_id"]
+
+
+assert pairwise_dists.shape[0] == len(signal_labels)
+
 # %%
 
-bgm = BayesianGaussianMixture(
-    n_components=params.bgm["n_components"],
-    max_iter=params.bgm["max_iter"],
-    random_state=params.seed,
-)
+speaker_ids = np.unique(signal_labels["speaker"])
 
-for speaker in pd.unique(df["speaker"]):
+dfs_to_save = []
+
+for speaker in speaker_ids:
     if speaker == "init":
         continue
-    cluster_labels = bgm.fit_predict(df[df["speaker"] == speaker][["mds_1", "mds_2"]])
-    df.loc[df["speaker"] == speaker, "cluster_label"] = cluster_labels
 
-df.to_csv("test/one2many_embedding_viz.csv", index=False)
+    indices = np.where(signal_labels["speaker"] == speaker)[0]
+    n_signals = len(indices)
 
-# If discreteness threshold is below a certain value, don't cluster
-# %%
+    signal_dists = pairwise_dists[np.ix_(indices, indices)]
+    # print(signal_dists)
+
+    for min_cluster_size in params.cluster_params["min_cluster_size"]:
+        hdbscan = HDBSCAN(
+            min_cluster_size=min_cluster_size,
+            metric="precomputed",
+            allow_single_cluster=True
+        ).fit(signal_dists)
+
+        labels = hdbscan.labels_
+
+        # medoids = np.zeros((n_signals, n_signals))
+        # for i in range(n_signals):
+        #     medoid_idx = np.argmin(np.mean(signal_dists[labels == i], axis=0))
+        #     medoids[i] = signal_dists[labels == i][medoid_idx]
+
+        # store results
+        df = signal_labels[signal_labels["speaker"] == speaker].copy()
+        df["min_cluster_size"] = min_cluster_size
+        df["cluster_label"] = labels
+
+        dfs_to_save.append(df)
+
+results = pd.concat(dfs_to_save).reset_index()
+results.to_csv("test_output/cluster_output.csv", index=False)
+
