a03b4abb4533407362cf00dff1e16a85b2cb8c9c
Alicia Chen <31644506+aliciamchen@users.noreply.github.com>
2024-06-28 15:55:58 -0400

    organize systematicity code

--- 06_general_syst.R
+++ 06_general_syst.R
@@ -3,10 +3,17 @@ library(tidyverse)
 library(energy)
 library(jsonlite)
 
-set.seed(111)  # For reproducibility
+set.seed(111)
 
-pairwise.dists <- as.matrix(read_table(here("test_output/pairwise_dists.txt"), col_names = FALSE))
-signal.labels <- as.data.frame(read_json(here("test_output/all_signal_labels.json"), simplifyVector = TRUE))
+args <- commandArgs(trailingOnly = TRUE)
+
+dists.file <- args[1]
+labels.file <- args[2]
+output.dir <- args[3]
+
+
+pairwise.dists <- as.matrix(read_table(here(dists.file), col_names = FALSE))
+signal.labels <- as.data.frame(read_json(here(labels.file), simplifyVector = TRUE))
 wcs <- read_json(here("tools/wcs_row_F.json"))
 
 euclidean <- function(a, b) sqrt(sum((a - b)^2))
@@ -29,18 +36,12 @@ for (id in speaker.ids) {
 
   for (i in 1:n.signals) {
     for (j in 1:n.signals) {
-      # print(signal.indices[i])
-      # print(signal.indices[j])
-      # print(pairwise.dists)
+
       signal.dist <- pairwise.dists[signal.indices[i], signal.indices[j]]
 
       color.idx.i <- strtoi(signal.labels[signal.indices[i], 4]) + 1 # correct for zero-indexing
       color.idx.j <- strtoi(signal.labels[signal.indices[j], 4]) + 1
-      # TODO: index by actual data frame title
 
-      # print(signal.labels[signal.indices[i], 3])
-      # print(color.idx.i)
-      # print(color.idx.j)
       color.dist = euclidean(unlist(wcs$luv[[color.idx.i]]), unlist(wcs$luv[[color.idx.j]]))
 
       signal.dists[i, j] <- signal.dist
@@ -53,15 +54,12 @@ for (id in speaker.ids) {
 
 
   ### Do permutation test
-  # Initialize a vector to store the results of the permutation test
-  permuted_dc <- numeric(1000)  # for 1000 permutations
+  permuted_dc <- numeric(10000)  # for 1000 permutations
 
   # Permutation test
-  for(i in 1:1000) {
-    # Shuffle the rows of the color_distances matrix
+  for(i in 1:10000) {
     shuffled.color.dists <- color.dists[sample(nrow(color.dists)), ]
 
-    # Calculate and store the distance correlation
     permuted_dc[i] <- dcor(signal.dists, shuffled.color.dists)
   }
 
@@ -76,22 +74,17 @@ for (id in speaker.ids) {
 colnames(systs) <- c('speaker', 'dcor', 'p')
 systs <- as.data.frame(systs)
 
-hist(as.numeric(systs[2:51, ]$p),
-     breaks = 100, # Adjusts the number of bins
-     col = "skyblue",
-     main = "Histogram of P-values",
-     xlab = "P-value",
-     border = "black")
+# hist(as.numeric(systs[2:51, ]$p),
+#      breaks = 100, # Adjusts the number of bins
+#      col = "skyblue",
+#      main = "Histogram of P-values",
+#      xlab = "P-value",
+#      border = "black")
 
-# Adding grid lines for better readability
-grid(nx = NULL, ny = NULL, col = "gray", lty = "dotted")
+# # Adding grid lines for better readability
+# grid(nx = NULL, ny = NULL, col = "gray", lty = "dotted")
 
-# ggplot(as.data.frame(systs), aes(x = p)) +
-#   geom_histogram(fill = "blue", color = "black", binwidth = 0.01, stat = "count") +
-#   theme_minimal() +
-#   ggtitle("Histogram of Values") +
-#   xlab("Values") +
-#   ylab("Frequency")
 
-write.csv(systs, here('test_output/syst_v2.csv'), row.names = FALSE)
+
+write.csv(systs, here(paste0(output.dir, "/metrics/systematicity.csv")), row.names = FALSE)
 
