2793fb5cb23ba7448aed1df4c880adff4b6a264b
Alicia Chen <31644506+aliciamchen@users.noreply.github.com>
2024-07-02 17:08:00 -0400

    uncomment

--- run_analysis.sh
+++ run_analysis.sh
@@ -2,12 +2,12 @@
 
 set -e
 
-# python 00_fetch_games.py --learn_raw raw_data/learning_data.zip --comm_raw raw_data/communication_game_data.zip --output_dir outputs
-# python 01_process_signals.py --learn_file outputs/learn.zip --comm_file outputs/comm.zip --output_dir outputs
-# python 02_pairwise_dists.py --init_signals outputs/init_signals_tidy.zip --comm_signals outputs/comm_signals_tidy.zip --output_dir outputs
-# python 03_embeddings.py --dists_file outputs/pairwise_dists.txt --labels_file outputs/signal_labels.json --output_dir outputs
+python 00_fetch_games.py --learn_raw raw_data/learning_data.zip --comm_raw raw_data/communication_game_data.zip --output_dir outputs
+python 01_process_signals.py --learn_file outputs/learn.zip --comm_file outputs/comm.zip --output_dir outputs
+python 02_pairwise_dists.py --init_signals outputs/init_signals_tidy.zip --comm_signals outputs/comm_signals_tidy.zip --output_dir outputs
+python 03_embeddings.py --dists_file outputs/pairwise_dists.txt --labels_file outputs/signal_labels.json --output_dir outputs
 
-# mkdir -p outputs/metrics
+mkdir -p outputs/metrics
 
 python 04_cluster.py --dists_file outputs/pairwise_dists.txt --labels_file outputs/signal_labels.json --output_dir outputs
 Rscript 05_general_syst.R outputs/pairwise_dists.txt outputs/signal_labels.json outputs/metrics
